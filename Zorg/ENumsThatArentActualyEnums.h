f33b684b45a68252c18ff622faac9c8043f9e003
Dreamer2345 <33360259+Dreamer2345@users.noreply.github.com>
2018-03-03 15:15:56 +0000

    Add files via upload

---
+++ Zorg/ENumsThatArentActualyEnums.h
@@ -0,0 +1,45 @@
+#pragma once
+
+
+#define MAXX 20
+#define MAXY 20
+
+uint8_t MAP[MAXX*MAXY];
+
+enum class GameState { 
+      MainMenu, 
+      GameStart,
+      NewLevel, 
+      Game ,
+      Dead ,
+};
+
+uint16_t GetOffset(uint8_t x,uint8_t y){
+  return (x+(y*MAXX))/2;
+}
+
+uint8_t GetBlock(uint8_t x,uint8_t y){
+ uint8_t Block = 0;
+ if (x % 2 == 0){
+    Block = MAP[GetOffset(x,y)] >> 4;
+ } else {
+    Block = MAP[GetOffset(x,y)] << 4;
+    Block = Block >> 4;
+ }
+ return Block;
+}
+
+void SetBlock(uint8_t x,uint8_t y,uint8_t b){
+ uint8_t Block = MAP[GetOffset(x,y)];
+ uint8_t AddBlock = b;
+ if (x % 2 == 0){
+    AddBlock = AddBlock << 4;
+    Block = Block << 4;
+    Block = Block >> 4;
+ } else {
+    Block = Block >> 4;
+    Block = Block << 4;
+ }
+ Block |= AddBlock;
+ MAP[GetOffset(x,y)] = Block;
+}
---
+++ Zorg/Zorg.ino
@@ -0,0 +1,22 @@
+#include <Arduboy2.h>
+#include "ArduboyBeep.h"
+Arduboy2 ard;
+Sprites sprites;
+BeepPin1 beep;
+#include "ENumsThatArentActualyEnums.h"
+#include "Game.h"
+
+void setup() {
+  ard.begin();
+  ard.clear();
+  ard.initRandomSeed();
+  SetBlock(0,1,5);
+  SetBlock(1,1,6);
+  ard.println(GetBlock(0,1));
+  ard.println(GetBlock(1,1));
+  ard.display();
+}
+
+void loop() {
+  ard.pollButtons();
+}
